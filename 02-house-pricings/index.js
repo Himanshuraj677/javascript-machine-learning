2e80ccb35956643a6f1cad5bf124a8b037d7ea5b
solygambas <51904909+solygambas@users.noreply.github.com>
2021-11-11 17:18:39 +0100

    add gpu and reference link

--- 02-house-pricings/index.js
+++ 02-house-pricings/index.js
@@ -1,4 +1,6 @@
+// Reference: https://www.tensorflow.org/js/guide/nodejs
 // require("@tensorflow/tfjs-node"); // CPU will be used for calculations
+// require("@tensorflow/tfjs-node-gpu"); // GPU will be used for calculations
 const tf = require("@tensorflow/tfjs");
 const loadCSV = require("./load-csv");
 
