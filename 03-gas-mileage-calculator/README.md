093ea9c68bfaf6a8812622bd8a16eca17889eb07
solygambas <51904909+solygambas@users.noreply.github.com>
2021-11-13 12:45:57 +0100

    add vectorized gradient descent

--- 03-gas-mileage-calculator/README.md
+++ 03-gas-mileage-calculator/README.md
@@ -7,5 +7,7 @@ A gradient descent algorithm using Tensorflow.js.
 - loading CSV data and creating a LinearRegression class.
 - creating a basic gradient descent implementation with arrays of data and for loops.
 - calculating mean squared error (MSE) slopes, updating coefficients and interpreting results.
+- understanding matrix multiplication.
+- vectorizing the gradient descent algorithm with Tensorflow.js helpers.
 
 Based on [Machine Learning with JavaScript](https://www.udemy.com/course/machine-learning-with-javascript/) by Stephen Grider (2020).
--- 03-gas-mileage-calculator/linear-regression.js
+++ 03-gas-mileage-calculator/linear-regression.js
@@ -3,45 +3,34 @@ const _ = require("lodash");
 
 class LinearRegression {
   constructor(features, labels, options) {
-    this.features = features;
-    this.labels = labels;
+    this.features = tf.tensor(features);
+    this.labels = tf.tensor(labels);
+    // Add a column of 1s to enable matrix multiplication
+    this.features = tf
+      .ones([this.features.shape[0], 1])
+      .concat(this.features, 1);
+
     this.options = Object.assign(
       { learningRate: 0.1, iterations: 1000 }, // fallback values if no options are provided
       options
     );
     // 1) Pick a value for m and b
-    this.m = 0;
-    this.b = 0;
+    this.weights = tf.zeros([2, 1]); // [[0],[0]];
   }
 
   gradientDescent() {
-    const currentGuessesForMPG = this.features.map((row) => {
-      return this.m * row[0] + this.b;
-    });
-
-    // 2a) Calculate slope of MSE with respect to b
-    const bSlope =
-      (_.sum(
-        currentGuessesForMPG.map((guess, i) => {
-          return guess - this.labels[i][0];
-        })
-      ) *
-        2) /
-      this.features.length;
-
-    // 2b) Calculate slope of MSE with respect to m
-    const mSlope =
-      (_.sum(
-        currentGuessesForMPG.map((guess, i) => {
-          return -1 * this.features[i][0] * (this.labels[i][0] - guess);
-        })
-      ) *
-        2) /
-      this.features.length;
+    // 2) Calculate slope of MSE with respect to m and b
+    // features * ((features * weights)) - labels) / n
+    const currentGuesses = this.features.matMul(this.weights);
+    const differences = currentGuesses.sub(this.labels);
+    const slopes = this.features
+      .transpose()
+      .matMul(differences)
+      .div(this.features.shape[0]);
+    // .mul(2) // unnecessary since we don't care about the exact value, only +/- is useful to interpret slope of MSE;
 
     // 3) and 4) Multiply both slopes by learning rates and substract results from b and m
-    this.m = this.m - mSlope * this.options.learningRate;
-    this.b = this.b - bSlope * this.options.learningRate;
+    this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
   }
 
   train() {
