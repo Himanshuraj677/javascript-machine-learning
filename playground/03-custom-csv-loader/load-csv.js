2f6cbc00f0d231db2a8aa08b77b8211b559a6f55
solygambas <51904909+solygambas@users.noreply.github.com>
2021-11-15 09:49:32 +0100

    add parsing values

---
+++ playground/03-custom-csv-loader/load-csv.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const _ = require("lodash");
+
+function loadCSV(filename, { converters = {} }) {
+  let data = fs.readFileSync(filename, { encoding: "utf-8" });
+  // if you need to remove '\r': https://stackoverflow.com/questions/21640902/remove-r-cr-from-csv
+  data = data.split("\n").map((row) => row.split(","));
+  // remove empty columns
+  data = data.map((row) => _.dropRightWhile(row, (val) => val === ""));
+  const headers = _.first(data);
+  data = data.map((row, index) => {
+    if (index === 0) {
+      return row;
+    }
+    return row.map((element, index) => {
+      // parse booleans
+      if (converters[headers[index]]) {
+        const converted = converters[headers[index]](element);
+        return _.isNaN(converted) ? element : converted;
+      }
+      // parse number values
+      const result = parseFloat(element);
+      return _.isNaN(result) ? element : result;
+    });
+  });
+  console.log(data);
+}
+loadCSV("data.csv", {
+  converters: { passed: (val) => (val === "TRUE" ? 1 : 0) },
+});
