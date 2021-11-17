dbbdc8cd9f48042d64b9353aad11668985b25307
solygambas <51904909+solygambas@users.noreply.github.com>
2021-11-17 09:47:01 +0100

    add encoded label values and accuracy gauge

--- 06-handwriting-recognition/index.js
+++ 06-handwriting-recognition/index.js
@@ -7,8 +7,31 @@ const _ = require("lodash");
 const plot = require("nodeplotlib");
 const mnist = require("mnist-data");
 
-const mnistData = mnist.training(0, 10);
-
+const mnistData = mnist.training(0, 5000);
 const features = mnistData.images.values.map((image) => _.flatMap(image));
+const encodedLabels = mnistData.labels.values.map((label) => {
+  const row = new Array(10).fill(0);
+  row[label] = 1; // row[7] = 1 if label value is 7
+  return row; // [ 0, 0, 0, 0, 0, 0, 0, 1, 0, 0  ],
+});
 
-console.log(features);
+const regression = new LogisticRegression(features, encodedLabels, {
+  learningRate: 1,
+  iterations: 20,
+  batchSize: 100,
+});
+
+regression.train();
+
+const testMnistData = mnist.testing(0, 100);
+const testFeatures = testMnistData.images.values.map((image) =>
+  _.flatMap(image)
+);
+const testEncodedLabels = testMnistData.labels.values.map((label) => {
+  const row = new Array(10).fill(0);
+  row[label] = 1; // row[7] = 1 if label value is 7
+  return row; // [ 0, 0, 0, 0, 0, 0, 0, 1, 0, 0  ],
+});
+
+const accuracy = regression.test(testFeatures, testEncodedLabels);
+console.log("Accuracy is", accuracy);
--- README.md
+++ README.md
@@ -117,5 +117,7 @@ Another multinominal logistic regression with a larger dataset.
 ### Features
 
 - flattening image data from MNIST handwritten digit database.
+- encoding label values.
+- implementing an accuracy gauge and debugging the calculation process.
 
 Based on [Machine Learning with JavaScript](https://www.udemy.com/course/machine-learning-with-javascript/) by Stephen Grider (2021).
